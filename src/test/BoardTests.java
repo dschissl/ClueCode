f84cecd5e4cc35d6f3bae2f2a4f32abc9627735c
unknown <dschissl@.adit.mines.edu>
2013-02-26 12:17:46 -0700

    Added new BoardTests test class

---
+++ src/test/BoardTests.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+import game.Board;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTests {
+	
+	@Before
+	public void setup() {
+		Board b = new Board();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
